0326919c3bb5dad445180c9ae8bdb1450f97427f
VortexCoffin <brittsamson7007@proton.me>
2026-07-10 15:16:43 -0600

    fix vite config syntax

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,12 +3,17 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    port: 5173,
-    strictPort: true,
-    proxy: {
-      '/api': 'http://127.0.0.1:5000',
-    },
-  },
+                            server: {
+                              host: true,
+                              port: 5173,
+                              strictPort: true,
+                              proxy: {
+                                '/api': 'http://127.0.0.1:5000',
+                              }
+                            },
+                            build: {
+                              target: 'es2020',
+                            outDir: 'dist',
+                            sourcemap: false
+                            }
 })
